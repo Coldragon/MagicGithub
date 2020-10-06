de2dc476b3169077d3681168b7cae3b08acfe909
coldragon <coldragon78@gmail.com>
2020-10-06 20:31:29 +0200

    Fix TU

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -2,11 +2,13 @@ package com.openclassrooms.magicgithub.api;
 
 import com.openclassrooms.magicgithub.model.User;
 import java.util.List;
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
+import java.util.Random;
+
+import com.openclassrooms.magicgithub.api.FakeApiServiceGenerator;
 
 public class FakeApiService implements ApiService {
 
-    private List<User> users = generateUsers();
+    private List<User> users = FakeApiServiceGenerator.generateUsers();
 
     /**
      * Return a list of {@link User}
@@ -14,8 +16,7 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public List<User> getUsers() {
-        // TODO: A modifier
-        return null;
+        return users;
     }
 
     /**
@@ -24,7 +25,13 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void generateRandomUser() {
-        // TODO: A modifier
+        Random rand = new Random();
+        users.add(
+                FakeApiServiceGenerator.FAKE_USERS_RANDOM.get(
+                        rand.nextInt(FakeApiServiceGenerator.FAKE_USERS_RANDOM.size()
+                        )
+                )
+        );
     }
 
     /**
@@ -32,6 +39,6 @@ public class FakeApiService implements ApiService {
      */
     @Override
     public void deleteUser(User user) {
-        // TODO: A modifier
+        users.remove(user);
     }
 }
